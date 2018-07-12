63cd9ded47726adf1863c8c5dcbe88f3df18718d
daybu <39197119+daybu@users.noreply.github.com>
2018-07-12 23:26:10 +0600

    Add files via upload

---
+++ p/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+ 
+
+
+
+$('.slider-active').owlCarousel({
+    loop:true,
+    dots:false,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+
+
+
+
+
+
+});
